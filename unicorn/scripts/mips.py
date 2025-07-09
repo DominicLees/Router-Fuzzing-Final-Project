7ef184ef4cecf08bb8156a1c0da3279714f30993
Dominic Lees <dxl184@student.bham.ac.uk>
2025-07-09 20:49:23 +0100

    Updated mips.py to successfully load target binary

--- unicorn/scripts/mips.py
+++ unicorn/scripts/mips.py
@@ -3,8 +3,13 @@ from unicorn import *
 from unicorn.mips_const import *
 import inspect
 
-ADDRESS = 0x100000
-ENTRY_POINT = 0x405da8
+CODE_ADDRESS = 0x00100000
+ENTRY_POINT = 0x402b30
+END_OF_MAIN = 0x402c64
+MAIN_SIZE = END_OF_MAIN - ENTRY_POINT
+END_ADDRESS = CODE_ADDRESS + MAIN_SIZE
+STACK_ADDRESS = 0x01000000
+STACK_SIZE = 0x00010000
 
 # callback for tracing basic blocks
 def hook_block(uc, address, size, user_data):
@@ -18,25 +23,32 @@ def hook_code(uc, address, size, user_data):
 with open("httpd", mode="rb") as file:
     binary_data = file.read()
 
+# initialise emulator in MIPS32 + EL mode
+mu = Uc(UC_ARCH_MIPS, UC_MODE_MIPS32 + UC_MODE_LITTLE_ENDIAN)
+
+# map 2MB memory for this emulation
+mu.mem_map(CODE_ADDRESS, 2 * 1024 * 1024)
+
+# write machine code to be emulated to memory
+mu.mem_write(CODE_ADDRESS, binary_data)
+
+# set program counter to start of program
+mu.reg_write(UC_MIPS_REG_PC, CODE_ADDRESS)
+
+# setup the stack
+mu.mem_map(STACK_ADDRESS, STACK_SIZE)
+mu.reg_write(UC_MIPS_REG_SP, STACK_ADDRESS + STACK_SIZE)
+
+# tracing all basic blocks with customized callback
+mu.hook_add(UC_HOOK_BLOCK, hook_block)
+
+# tracing all instructions with customized callback
+mu.hook_add(UC_HOOK_CODE, hook_code)
+
 print("Starting emulation")
 try:
-    # initialise emulator in MIPS32 + EL mode
-    mu = Uc(UC_ARCH_MIPS, UC_MODE_MIPS32 + UC_MODE_LITTLE_ENDIAN)
-
-    # map 2MB memory for this emulation
-    mu.mem_map(ADDRESS, 2 * 1024 * 1024)
-
-    # write machine code to be emulated to memory
-    mu.mem_write(ADDRESS, binary_data)
-
-    # tracing all basic blocks with customized callback
-    mu.hook_add(UC_HOOK_BLOCK, hook_block)
-
-    # tracing all instructions with customized callback
-    mu.hook_add(UC_HOOK_CODE, hook_code)
-
     # emulate machine code in infinite time
-    mu.emu_start(ENTRY_POINT, ENTRY_POINT + len(binary_data))
+    mu.emu_start(mu.reg_read(UC_MIPS_REG_PC), END_ADDRESS)
 except UcError as e:
     print("ERROR: %s" % e)
     print("Last function called: %s" % inspect.trace()[-1][3])
