90b1d7eaab83b610f381b4c98a5a023f04d35cc1
Dominic Lees <dxl184@student.bham.ac.uk>
2025-08-05 18:56:37 +0000

    Changed the implementation of sscanf in http_parseDescStr.py to correctly emulate its behaviour

--- unicorn/scripts/http_parseDescStr.py
+++ unicorn/scripts/http_parseDescStr.py
@@ -1,25 +1,37 @@
 from mips import *
 from unicorn.mips_const import *
+from parse import parse, Result
 
 def hook_skip_printf(uc: Uc, address: int, size: int, user_data):
-    uc.reg_write(UC_MIPS_REG_PC, 0x40b178)
+    uc.reg_write(UC_MIPS_REG_PC, address + 0x10)
 
 # implement strchr
 def hook_strchr(uc: Uc, address: int, size: int, user_data):
     uc.reg_write(UC_MIPS_REG_V0, uc.reg_read(UC_MIPS_REG_A0))
     uc.reg_write(UC_MIPS_REG_PC, 0x40b148)
 
-# implement scanf
-def hook_scanf(uc: Uc, address: int, size: int, user_data):
-    uc.reg_write(UC_MIPS_REG_V0, 12)
-    uc.mem_write(uc.reg_read(UC_MIPS_REG_A2), b"\01")
-    seventh_int_address = int.from_bytes(uc.mem_read(uc.reg_read(UC_MIPS_REG_SP) + 0x20, 4), "little")
-    uc.mem_write(seventh_int_address, b"\00\00\00\07")
+# implement sscanf
+def hook_sscanf(uc: Uc, address: int, size: int, user_data: str):
+    result = parse("#{:d},{:d},{:d},{:d},{:d},{:d}#{:d},{:d},{:d},{:d},{:d},{:d}", user_data)
+    # if string does not match format return 0
+    if type(result) is not Result:
+        uc.reg_write(UC_MIPS_REG_V0, 0)
+        return
+    result = result.fixed
+    uc.reg_write(UC_MIPS_REG_V0, len(result))
+    SP = uc.reg_read(UC_MIPS_REG_SP)
+    # first 6 numbers stored consecutively in stack starting at 0x58, next 6 numbers stored consecutively starting at 0x40
+    for i in range(0,6):
+        uc.mem_write(SP + 0x58 + i * 4, int.to_bytes(result[i], byteorder="little"))
+    for i in range(0,6):
+        uc.mem_write(SP + 0x40 + i * 4, int.to_bytes(result[6 + i], byteorder="little"))
 
+# expected arg1 format: #%u,%u,%u,%u,%u,%u#%u,%u,%u,%u,%u,%u
+arg1 = "#1,1,1,1,1,1#1,1,1,1,1,1"
 # run emulation
 emu = Emu("/workspaces/dxl184/unicorn/includes/httpd", 0x40b0b4, 0x40b2e0, True)
 emu.add_hook(0x40b168, hook_skip_printf)
+emu.add_hook(0x40b204, hook_skip_printf)
 emu.add_hook(0x40b13c, hook_strchr)
-emu.add_hook(0x40b1ec, hook_scanf)
-# expected arg1 format: #%u,%u,%u,%u,%u,%u#%u,%u,%u,%u,%u,%u
-emu.run(["#1,1,1,1,1,1#7,1,1,1,1,1", bytes(4), bytes(4), bytes(4)])
+emu.add_hook(0x40b1ec, hook_sscanf, arg1)
+emu.run([arg1, bytes(4), bytes(2), bytes(2)])
