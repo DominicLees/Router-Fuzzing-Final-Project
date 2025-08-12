b6983c73d25dcee52719e287180f9b0334b412e9
Dominic Lees <dxl184@student.bham.ac.uk>
2025-08-12 20:00:00 +0000

    Setup http_parseDescStr.py to use Emu.fuzz()

--- unicorn/scripts/http_parseDescStr.py
+++ unicorn/scripts/http_parseDescStr.py
@@ -1,6 +1,7 @@
 from mips import *
 from unicorn.mips_const import *
 from parse import parse, Result
+import random
 
 def hook_skip_printf(uc: Uc, address: int, size: int, user_data):
     uc.reg_write(UC_MIPS_REG_PC, address + 0x10)
@@ -26,12 +27,20 @@ def hook_sscanf(uc: Uc, address: int, size: int, user_data: str):
     for i in range(0,6):
         uc.mem_write(SP + 0x40 + i * 4, int.to_bytes(result[6 + i], length=4, byteorder="little"))
 
-# expected arg1 format: #%u,%u,%u,%u,%u,%u#%u,%u,%u,%u,%u,%u
-arg1 = "#1,1,1,1,1,1#1,1,1,1,1,1"
 # run emulation
-emu = Emu("/workspaces/dxl184/unicorn/includes/httpd", 0x40b0b4, 0x40b2e0, True)
+emu = Emu("/workspaces/dxl184/unicorn/includes/httpd", 0x40b0b4, 0x40b2e0)
 emu.add_hook(0x40b168, hook_skip_printf)
 emu.add_hook(0x40b204, hook_skip_printf)
 emu.add_hook(0x40b13c, hook_strchr)
-emu.add_hook(0x40b1ec, hook_sscanf, arg1)
-emu.run([arg1, bytes(4), bytes(2), bytes(2)])
+
+# generate arg1, expected arg1 format: #%u,%u,%u,%u,%u,%u#%u,%u,%u,%u,%u,%u
+def generate_args():
+    nums = []
+    for i in range(0, 12):
+        nums.append(random.randint(0, 5))
+    return ["#%u,%u,%u,%u,%u,%u#%u,%u,%u,%u,%u,%u" % tuple(nums), bytes(4), bytes(2), bytes(2)]
+
+def pre_run(emu: Emu, args: list):
+    emu.add_hook(0x40b1ec, hook_sscanf, args[0])
+
+emu.fuzz(generate_args, pre_run_function=pre_run)
