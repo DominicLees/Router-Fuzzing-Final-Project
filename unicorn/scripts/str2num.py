2fb8d54787c46de3eab8f23840aef523a07543dc
Dominic Lees <dxl184@student.bham.ac.uk>
2025-08-13 13:44:18 +0000

    Converted str2num.py to use Emu.fuzz()

--- unicorn/scripts/str2num.py
+++ unicorn/scripts/str2num.py
@@ -1,20 +1,9 @@
 from mips import Emu
 import os
 
-# constants are currently setup to run str2num from the httpd binary from the TL-WR841N router
-ENTRY_POINT = 0x412480
-END_OF_FUNCTION = 0x4124b4
+# currently setup to run str2num from the httpd binary from the TL-WR841N router
+def arg_gen():
+    return os.urandom(999) + b"\00"
 
-emu = Emu("httpd", ENTRY_POINT, END_OF_FUNCTION)
-count = 0
-
-# Continuously executes the function until unicorn throws an error or stopped by the user
-while (True):
-    try:
-        count += 1
-        result = emu.run(os.urandom(999) + b"\00") # Pass 999 random bytes to the function
-        if result < 0:
-            break
-    except KeyboardInterrupt:
-        print("\nExecutions: %d" % count)
-        exit(0)
+emu = Emu("/workspaces/dxl184/unicorn/includes/httpd", 0x412480, 0x4124b4)
+emu.fuzz(arg_gen)
