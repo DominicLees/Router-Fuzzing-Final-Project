ab8b849f7fe69f73dde42759f0fdaf7bac700325
Dominic Lees <dxl184@student.bham.ac.uk>
2025-08-13 16:45:03 +0000

    Deleted fuzz.py

--- unicorn/scripts/fuzz.py
+++
@@ -1,9 +0,0 @@
-from unicorn.scripts.emu import *
-
-# constants are currently setup to run str2num from the httpd binary from the TL-WR841N router
-ENTRY_POINT = 0x412480
-END_OF_FUNCTION = 0x4124b4
-STRING = b"123\00"
-
-emu = Emu("httpd", ENTRY_POINT, END_OF_FUNCTION)
-emu.run(STRING)
