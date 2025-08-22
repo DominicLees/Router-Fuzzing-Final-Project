027e3f63be503e0798c1deeea27e37dc1d190ab8
Dominic Lees <dxl184@student.bham.ac.uk>
2025-08-22 12:52:58 +0000

    Converted example.py to a fuzzing script

--- unicorn/scripts/example.py
+++ unicorn/scripts/example.py
@@ -4,10 +4,10 @@ from unicorn.mips_const import *
 import os, random
 
 def argument_generator():
-    return os.urandom(1000)
+    return [os.urandom(random.randint(1, 33))]
 
-def hook_strcpy(uc: Uc, address: int, size: int, user_data: str):
-    uc.mem_write(uc.reg_read(UC_MIPS_REG_A0), bytes(user_data, "ascii") + b'\00')
+def hook_strcpy(uc: Uc, address: int, size: int, user_data: bytes):
+    uc.mem_write(uc.reg_read(UC_MIPS_REG_A0), user_data)
     uc.reg_write(UC_MIPS_REG_V0, 0)
 
 def hook_strcmp(uc: Uc, address: int, size: int, user_data: str):
@@ -16,10 +16,10 @@ def hook_strcmp(uc: Uc, address: int, size: int, user_data: str):
     else:
         uc.reg_write(UC_MIPS_REG_V0, 1)
 
-emu = Emu("/workspaces/dxl184/unicorn/scripts/example_targets/vulnerable_target.example", 0x400710, 0x4007d4, True)
-input = "password"
-emu.add_hook(0x400750, hook_strcpy, input)
-emu.add_hook(0x400774, hook_strcmp, input)
-emu.setup()
-emu.uc.mem_write(0x42003c, int.to_bytes(0x42003c, 4, "little"))
-emu.run(input, False)
+def pre_run(emu: Emu, args: list[bytes]):
+    emu.add_hook(0x400750, hook_strcpy, args[0])
+    emu.add_hook(0x400774, hook_strcmp, args[0].decode("ascii", "replace"))
+    emu.uc.mem_write(0x42003c, int.to_bytes(0x42003c, 4, "little"))
+
+emu = Emu("/workspaces/dxl184/unicorn/scripts/example_targets/vulnerable_target.example", 0x400710, 0x4007d4)
+emu.fuzz(argument_generator, pre_run_function=pre_run)
